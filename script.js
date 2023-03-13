5cb77d6491116d0cca8afcc0a8defc6431583899
james <james@devpipeline.com>
2023-03-13 09:53:35 -0600

    changed from rest to results

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ Catch errors appropriately.
 */
 
 fetch("https://swapi.tech/api/people/1")
-  .then(rest => rest.json())
-  .then(jsonData => jsonData.result.properties.name)
-  .then(grabbedData => document.body.appendChild(document.createTextNode(grabbedData)))
-  .then(data => console.log(data))
-  .catch(err => console.error(err))
+  .then((results) => results.json())
+  .then((jsonData) => jsonData.result.properties.name)
+  .then((grabbedData) =>
+    document.body.appendChild(document.createTextNode(grabbedData))
+  )
+  .then((data) => console.log(data))
+  .catch((err) => console.error(err));
